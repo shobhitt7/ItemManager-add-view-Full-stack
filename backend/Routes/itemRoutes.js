e993569eb1f016d8d81106f5de17f4d8df70f88f
shobhitt7 <shobhitgs2011@gmail.com>
2025-06-22 19:59:07 +0530

    bug fixing

--- backend/Routes/itemRoutes.js
+++ backend/Routes/itemRoutes.js
@@ -7,17 +7,13 @@ const router = express.Router();
 // this is multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads');
+    cb(null, 'uploads') 
   },
   filename: (req, file, cb) => {
-    const originalName = file.originalname
-      .replace(/\s+/g, '-')           // Replace spaces with hyphens
-      .replace(/[^\w.-]/g, '')        // Remove special characters
-    const uniqueName = Date.now() + '-' + originalName;
+    const uniqueName = Date.now() + '-' + file.originalname;
     cb(null, uniqueName);
-  }
-});
-
+  },
+})
 
 const upload = multer({ storage })
 
