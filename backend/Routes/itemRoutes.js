2489c39ed7fb9d46d1f75d00d6e750418c58217f
shobhitt7 <shobhitgs2011@gmail.com>
2025-06-22 14:21:39 +0530

    bug fixing

--- backend/Routes/itemRoutes.js
+++ backend/Routes/itemRoutes.js
@@ -27,17 +27,10 @@ router.post('/', upload.fields([
 
     const { name, type, description } = req.body
 
-    const coverImagePath = req.files['coverImage']?.[0]
-  ? `${req.protocol}://${req.get('host')}/${req.files['coverImage'][0].path.replace(/\\/g, '/')}`
-  : null
-
-const additionalImagePaths = req.files['additionalImages']
-  ? req.files['additionalImages'].map(file =>
-      `${req.protocol}://${req.get('host')}/${file.path.replace(/\\/g, '/')}`
-    )
-  : []
-
-
+    const coverImagePath = req.files['coverImage']?.[0].path
+    const additionalImagePaths = req.files['additionalImages']
+      ? req.files['additionalImages'].map(file => file.path)
+      : []
 
     const newItem = new Item({
       name,
